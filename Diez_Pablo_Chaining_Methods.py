86dc0969dbf13cadebad9d9876f6ece418baf61b
Pablo X <jprodsanz@gmail.com>
2022-08-12 16:39:03 -0500

    hello

---
+++ Diez_Pablo_Chaining_Methods.py
@@ -0,0 +1,33 @@
+class User: 
+    
+    def __init__(self, first_name, last_name, email, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email = email
+        self.age = age
+        self.is_rewards_member = False
+        self.gold_card_points = 0
+
+    def display_info(self):
+        print(f'first_name:{self.first_name}')
+        print(f'last_name:{self.last_name}')
+        print(f'email:{self.email}')
+        print(f'age:{self.age}')
+        print(f'is_rewards_member:{self.is_rewards_member}')
+        print(f'gold_card_points:{self.gold_card_points}')
+        print("=================")
+        
+    def enroll(self):
+        self.is_rewards_member = True
+        self.gold_card_points = 200
+
+    def spend_points(self, amount):
+        self.gold_card_points = self.gold_card_points - amount
+
+my_user = User('Elmina', 'Brkic','ebrkic@gmail.com', 99)
+my_user.display_info().enroll().display_info().spend_points().display_info()
+
+
+
+
+
